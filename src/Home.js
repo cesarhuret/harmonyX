4b634169dab42c1222d7271821d9eb707b47ba06
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-11-02 13:55:00 +0000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,81 +4,88 @@ import ReactDOM from 'react-dom';
 
 export default class Home extends Component {
 
-  async componentDidMount () {
-
-    document.getElementById('messageinput').addEventListener("keyup", async (event) => {
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13 && event.target.value !== "") {
-          // Cancel the default action, if needed
-            const message = await fetch('https://chat.kesarx.repl.co/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    sender: 'anonymous',
-                    content: event.target.value 
-                })
-            }).then((res) => res.json());
-            this.renderMessages()
-            document.getElementById('messageinput').value = ''
-        }
-      });
-
-    this.renderMessages()
-  }
-
-  async renderMessages() {
-    let messageList;
-    try {
-        const messages = await fetch('https://chat.kesarx.repl.co/')
-        messageList = await messages.json();
-    } catch (e) {
-            console.log(e)
+    constructor () {
+        super()
+        this.socket = new WebSocket('ws://chat.kesarx.repl.co')
+        this.waitForConnection = this.waitForConnection.bind(this)
+        this.sendMessage = this.sendMessage.bind(this)
+        this.itemsList = [];
     }
 
-        let itemsList = [];
-        for (let i = 0; i < messageList.length; i++) {
-            const date = new Date(messageList[i].updatedAt)
-                itemsList.push(
-                    <div key={messageList[i]._id}>
+    async componentDidMount () {
+
+        this.socket.addEventListener('open', function(event) {
+            console.log('connected to ws')
+        })
+
+        this.socket.addEventListener('message', event => {
+            this.itemsList.push(`${event.data}`)
+            this.renderChat()
+        })
+
+        document.getElementById('messageinput').addEventListener("keyup", async (event) => {
+            // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13 && event.target.value !== "") {
+                this.sendMessage(event.target.value)
+                document.getElementById('messageinput').value = ''
+            }
+        });
+
+    }
+
+    sendMessage (message, callback) {
+        this.waitForConnection( () => {
+            this.socket.send(message)
+            if (typeof callback !== 'undefined') {
+              callback();
+            }
+        }, 1000);
+    };
+
+    waitForConnection (callback, interval) {
+        if (this.socket.readyState == 1) {
+            callback();
+        } else {
+            var that = this;
+            // optional: implement backoff for interval here
+            setTimeout(function () {
+                that.waitForConnection(callback, interval);
+            }, interval);
+        }
+    };
+
+    renderChat() {
+        const messages = (
+            <div>
+                {this.itemsList.map((item) => (
+                    <div>
                         <Col style={{paddingBottom: 30}}>
                             <Card bg='dark' text='white'>
-                                <Card.Header>
-                                    <Row>
-                                        <Col xs={{span: 'auto'}}>{messageList[i].sender}</Col>
-                                        <Col xs={{span: 'auto'}} text='muted' className='ml-auto'>{date.toLocaleDateString()} {date.toLocaleTimeString()}</Col>
-                                        <img src='delete.png' onClick={async () => {
-                                            await fetch(`https://chat.kesarx.repl.co/delete/${messageList[i]._id}`, {
-                                                method: 'DELETE',
-                                            })
-                                            this.renderMessages()
-                                        }}/>
-                                    </Row>
-                                </Card.Header>
                                 <Card.Body>
-                                    <Card.Text>{messageList[i].content}</Card.Text>
+                                    <Card.Text>{item}</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
-                    </div>
-                )
-        }
-      ReactDOM.render(itemsList, document.getElementById('chat'))
-  }
+                    </div>                        
+                ))}
+            </div>
+        )
+        ReactDOM.render(messages, document.getElementById('chat'))
+    }
 
-  render() {
+    render() {
         return ( 
         <div className="App">
             <Container>
                     <Container className='rounded' style={{textAlign: "left", paddingTop: '5%', paddingBottom: '5%'}}>
-                        <div className='p-4' id='chat' style={{border: '2px solid black',  borderRadius: '1.5rem',}}></div>
+                        <div className='p-4' id='chat' style={{border: '2px solid black',  borderRadius: '1.5rem',}}>
+                        
+                        </div>
                     </Container>
                 <div className='fixed-bottom'>
                     <Container className='rounded' style={{textAlign: "left", backgroundColor: 'rgba(40, 40, 40, 0.8)', padding: '20px'}}>
                         <div className="mb-3">
-                                <input id='messageinput' className="form-control form-control-user inputfocus" type="text" placeholder="Message"/>
-                        </div>
+                                <input id='messageinput' className="form-control form-control-user inputfocus" type="text" placeholder="Message"/>                        </div>
                     </Container>
                 </div>
             </Container>
