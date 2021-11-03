0e54bac64b63f71d61ef3d7526d4cee4a9beb63e
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-11-03 15:55:28 +0000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Card, Col, Container } from "react-bootstrap";
+import { Card, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import ReactDOM from 'react-dom';
 import './App.css'
 
@@ -67,13 +67,14 @@ export default class Chat extends Component {
         const messages = (
             <div>
                 {this.itemsList.map((item, index) => (
-                    <div id={index} key={index}>
+                    <Row id={index} key={index}>
+                        <span className='ml-auto' style={{backgroundColor: `#${item.color}`, color: `#${item.color}`}}>| </span>
                         <Col>
-                            <Card className='border-0 p-2' style={{backgroundColor: `#${item.color}`, borderRadius: '0'}}>
-                                    <Card.Text>{item.message}</Card.Text>
+                            <Card className='border-0' style={{backgroundColor: `transparent`, borderRadius: '0'}}>
+                                    <Card.Text style={{color: 'white'}}>{item.message}</Card.Text>
                             </Card>
                         </Col>
-                    </div>                        
+                    </Row>                        
                 ))}
             </div>
         )
@@ -84,16 +85,27 @@ export default class Chat extends Component {
     render() {
         return ( 
         <div className="App">
-            <Container>
+            <div className="fixed-top">
+                <Navbar bg='none' variant="dark" expand="lg" fixed='top' style={{ position: "sticky", top: 0, backgroundColor: 'transparent'}}>
+                <Container className="align-items-center" fluid>
+                    <Navbar.Brand href={window.location.pathname}>
+                        <Col className="mb-3"><h1 style={{textDecoration: 'underline'}}>{window.location.pathname}</h1></Col>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-white">
+                        <Nav.Link className='navlink2'>Rooms</Nav.Link>
+                        <Nav.Link className='navlink2'>{this.state.activeUsers}</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+                </Navbar>
+            </div>
+            <Container fluid>
                     <Container className='rounded' style={{textAlign: "left", paddingTop: '5%', paddingBottom: '5%'}}>
                         <div className='p-4' id='chat'>
                         </div>
                     </Container>
-                <div className='fixed-top'>
-                    <Container className='rounded' style={{textAlign: "left", color: 'white', backgroundColor: 'rgba(40, 40, 40, 0.8)', padding: '20px'}}>
-                    <Col className="mb-3"><p>{this.state.activeUsers}</p></Col>
-                    </Container>
-                </div>
                 <div className='fixed-bottom'>
                     <Container className='rounded' style={{textAlign: "left", backgroundColor: 'rgba(40, 40, 40, 0.8)', padding: '20px'}}>
                             <Col className="mb-3">
